32261ed625352289d58bbf349744ffcdedf7600c
JTAhlbrecht <55859141+JTAhlbrecht@users.noreply.github.com>
2025-07-09 16:56:01 -0500

    Added folders for models and textures Added custom item info from Kaupenjoe tutorial

--- src/main/java/com/shadowmaim/dirthurter/item/ModItems.java
+++ src/main/java/com/shadowmaim/dirthurter/item/ModItems.java
@@ -1,11 +1,16 @@
 package com.shadowmaim.dirthurter.item;
 
 import com.shadowmaim.dirthurter.Dirthurter;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Dirthurter.MODID);
+
+    public static final DeferredItem<Item> DIRTHURTER = ITEMS.register("dirthurter",
+            () -> new Item(new Item.Properties()));
 
     public static  void register(IEventBus eventBus) {
         ITEMS.register(eventBus);

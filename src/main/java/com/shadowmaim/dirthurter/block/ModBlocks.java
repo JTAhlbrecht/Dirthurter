77afdd78f17d6b4b4c1b673af0496b5fb106326d
JTAhlbrecht <55859141+JTAhlbrecht@users.noreply.github.com>
2025-08-20 16:04:54 -0500

    Update ModBlocks.java

--- src/main/java/com/shadowmaim/dirthurter/block/ModBlocks.java
+++ src/main/java/com/shadowmaim/dirthurter/block/ModBlocks.java
@@ -1,4 +1,13 @@
 package com.shadowmaim.dirthurter.block;
 
+import com.shadowmaim.dirthurter.Dirthurter;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Dirthurter.MODID);
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
